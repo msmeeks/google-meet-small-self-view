2a5c1c8bb8eb042cd80d9cfe94c62ac37ec3b712
msmeeks <msmeeks1@gmail.com>
2021-08-06 17:39:54 -0400

    Initial commit with v 0.1 script

---
+++ script.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name         Google Meet Small Self-View
+// @namespace    github.com/msmeeks/google-meet-small-self-view
+// @version      0.1
+// @description  Acuomatically shrink the self-view thumbnail in google.meet and move it to the top left corner
+// @author       msmeeks
+// @match        https://meet.google.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function resize() {
+        var width = '100px',
+            height = '50px';
+
+        var thumbnail = document.querySelector('div.ADivge');
+        thumbnail.style.width = width;
+        thumbnail.style.height = height;
+
+        var vid = thumbnail.querySelector('video');
+        vid.style.width = width;
+        vid.style.height = height;
+
+        var container = thumbnail.parentElement.parentElement;
+        container.style.position = 'fixed';
+        container.style.top = '-10px';
+        container.style.right = '-10px';
+
+        var label = thumbnail.querySelector('[data-self-name]');
+        label.style.visibility = 'hidden';
+    };
+
+    setInterval(resize, 5000);
+})();
